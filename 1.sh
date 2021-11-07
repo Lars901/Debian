58044e33b3a1de0ebd3129345cdccb6125550b5b
Lars901 <larsove_hoines@outlook.com>
2021-11-07 22:48:28 +0100

    Change release

--- 1.sh
+++ 1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Installing Kde-plasma minimal,fonts and Brave-browser dependencies"
-sudo apt -y install kde-plasma-desktop plasma-nm apt install fonts-terminus neofetch apt-transport-https curl wget git gdebi-core python3-pip
+sudo apt -y install kde-plasma-desktop plasma-nm fonts-terminus neofetch apt-transport-https curl wget git gdebi-core python3-pip
 
 
 
