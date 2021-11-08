0016b7ac52ccd29166f6a150ad09c80988933957
Lars901 <larsove_hoines@outlook.com>
2021-11-08 21:11:00 +0100

    Increase font size and change console font

--- 0.sh
+++ 0.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 sudo nano /etc/apt/sources.list
 sudo apt -y update && upgrade 
+sudo apt -y install terminus-font
+sudo dpkg-reconfigure console-setup
--- 1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sudo apt update
-echo "Installing Kde-plasma minimal,fonts and Brave-browser dependencies"
-sudo apt -y install kde-plasma-desktop plasma-nm fonts-terminus neofetch apt-transport-https curl wget git gdebi-core python3-pip
-
-
-
