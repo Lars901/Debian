8d841842a7bc57b856629af5d187480073d79dfa
Lars901 <larsove_hoines@outlook.com>
2021-11-17 19:11:27 +0100

    Update Github-desktop.sh

--- Github-desktop.sh
+++ Github-desktop.sh
@@ -1,4 +1,5 @@
-wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
+sudo apt install wget
+etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftkey-desktop.list'
 sudo apt-get update
 sudo apt install github-desktop
