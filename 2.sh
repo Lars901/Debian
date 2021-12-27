b24107daa180c6fbb00221adf4dd6fa99565f448
Lars901 <larsove_hoines@outlook.com>
2021-12-27 16:00:46 +0100

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -18,7 +18,7 @@ sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/
 sudo apt-get update
 sudo apt install github-desktop
 sudo apt update
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update
 sudo apt install spotify-client
