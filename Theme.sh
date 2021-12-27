5a78494536e02c5f7e300d0efdad58a85d2f3498
Lars901 <larsove_hoines@outlook.com>
2021-12-27 15:24:05 +0100

    move

---
+++ Theme.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Download Nordic Theme
+cd /usr/share/themes/
+git clone https://github.com/EliverLara/Nordic.git
+
+# Fira Code Nerd Font variant needed
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d ~/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d ~/.fonts   
+fc-cache -vf
+
+#Ms-fonts
+sudo apt install ttf-mscorefonts-installer
+
+# Layan Cursors
+cd "$HOME/build"
+git clone https://github.com/vinceliuice/Layan-cursors
+cd Layan-cursors
+sudo ./install.sh
+
+echo "RUN LXAPPEARANCE"
+cd ../
+cp .Xresources ~
+cp .Xnord ~
+cp -R dotfiles/* ~/.config/
