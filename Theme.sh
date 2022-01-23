c9a9c433809fb88b7efc18c44a55f9ccdbbf7b73
Lars901 <larsove_hoines@outlook.com>
2022-01-23 18:23:45 +0100

    Update Theme.sh

--- Theme.sh
+++ Theme.sh
@@ -5,7 +5,7 @@ cd /usr/share/themes/
 git clone https://github.com/EliverLara/Nordic.git
 
 # Fira Code Nerd Font variant needed
-cd "$HOME/Downloads
+cd /home
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip -d ~/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
@@ -16,7 +16,7 @@ fc-cache -vf
 sudo apt install ttf-mscorefonts-installer
 
 # Layan Cursors
-cd /larsove/home
+cd /home
 git clone https://github.com/vinceliuice/Layan-cursors
 cd Layan-cursors
 sudo ./install.sh
