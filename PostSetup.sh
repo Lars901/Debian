d1e338353432959284e3a69ce8a4d40efac83d3e
Lars901 <larsove_hoines@outlook.com>
2023-04-11 19:16:59 +0200

    Update PostSetup.sh

--- PostSetup.sh
+++ PostSetup.sh
@@ -177,7 +177,7 @@ flatpak install -y flathub com.spotify.Client
 #Dolphin Emu
 flatpak install -y flathub org.DolphinEmu.dolphin-emu
 cd "/home/$username"
-#wget Windwaker tbt
+#wget https://downloads.romspedia.com/roms/Legend%20of%20Zelda%2C%20The%20-%20The%20Wind%20Waker%20%28USA%29.7z
 
 #RPCS3 Emu
 flatpak install -y flathub net.rpcs3.RPCS3
